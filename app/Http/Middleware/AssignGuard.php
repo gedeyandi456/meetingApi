cc8f4bc166ce1dbdae9df19d8c5ad4e20f74d79c
gedeyandi456 <gede.yandi@blockchainzoo.com>
2019-04-22 11:44:31 +0800

    create and configure AssignGuard Middleware

---
+++ app/Http/Middleware/AssignGuard.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AssignGuard
+{
+    public function handle($request, Closure $next, $guard = null)
+    {
+        if($guard == null) {
+            auth()->shouldUse($guard);
+        }
+        
+        return $next($request);
+    }
+}
